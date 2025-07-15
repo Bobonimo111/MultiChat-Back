37f97c34ce273709442380cdffce317213f5ed5c
bobonimo <carlosmaycon443@gmail.com>
2025-07-15 10:08:59 -0300

    firt commit

---
+++ src/main/java/com/william/chat/mult/ChatMultApplication.java
@@ -0,0 +1,13 @@
+package com.william.chat.mult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatMultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatMultApplication.class, args);
+	}
+
+}
